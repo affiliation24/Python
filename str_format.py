e88efbe5e740f121de4f71a2c150cfebafc3bbe5
Madjay <118521001+affiliation24@users.noreply.github.com>
2025-01-30 12:41:22 +0300

    first commit

---
+++ str_format.py
@@ -0,0 +1,11 @@
+age = 26
+name = 'Swaroop'
+
+print("Возраст {0} -- {1} лет.". format(name, age))
+print("Спроси у {0} \n". format(name))
+
+print('Выводит десятичное число (.) с точностью в 3 знака {0:.3}'.format(0.543551565626))
+print('Результат деления 12 на 7 , с точностью в 8 знака {0:.8} \n'.format(12/7))
+
+# по ключевым словам:
+print('{name} написал {book}'.format(name='Swaroop', book='A Byte of Python'))
